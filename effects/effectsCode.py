ec1cda359f8b863b542f3d874d4a6a08948a25f9
Rasheed Lewis <rasheedlewis1997@gmail.com>
2015-11-10 17:34:08 -0500

    Update effectsCode.py

--- effects/effectsCode.py
+++ effects/effectsCode.py
@@ -1,7 +1,7 @@
 #Tristen Allen
 #tristen.allen@gatech.edu 903035156
 #Rasheed Lewis
-#email gtid
+#rlewis67@gatech.edu 903138303
 #We worked on this homework assignment alone,
 #using only this semester's course materials.
 
